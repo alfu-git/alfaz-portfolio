466ccff6381640ceee08b6b373f4291bdc460b39
alfu-git <mdalfaz.dev@gmail.com>
2026-05-12 02:17:42 +0600

    set animated bg using tsparticles

---
+++ src/components/shared/particlesBg/ParticlesWrapper.jsx
@@ -0,0 +1,9 @@
+"use client"
+import React from 'react';
+import ParticlesBg from './ParticlesBg';
+
+const ParticlesWrapper = () => {
+  return <ParticlesBg />
+};
+
+export default ParticlesWrapper;
